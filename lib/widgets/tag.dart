37acf3995cf353f9c09a7f7b58424f9b81581cb5
Iris Veress <irisveress@gmail.com>
2024-08-11 02:18:45 +0300

    tag selection animation

--- lib/widgets/tag.dart
+++ lib/widgets/tag.dart
@@ -1,52 +1,82 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class Tag extends StatefulWidget {
   const Tag({super.key, required this.name});
 
   final String name;
-  final Color backgroundColor = CupertinoColors.darkBackgroundGray;
 
   @override
   _TagState createState() => _TagState();
 }
 
 class _TagState extends State<Tag> {
+  Color backgroundColor = CupertinoColors.darkBackgroundGray;
+  Color textColor = CupertinoColors.systemGrey;
+  bool isSelected = false;
+  int transitionDuration = 100;
+
   @override
   Widget build(BuildContext context) {
-    return ConstrainedBox(
-      constraints: const BoxConstraints(minWidth: 85, maxWidth: 120),
-      child: Container(
-        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-        margin: const EdgeInsets.only(bottom: 12, left: 4, right: 4),
-        decoration: BoxDecoration(
-          color: widget.backgroundColor,
-          borderRadius: BorderRadius.circular(20),
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          isSelected = !isSelected;
+          backgroundColor = isSelected
+              ? CupertinoColors.activeBlue
+              : CupertinoColors.darkBackgroundGray;
+          textColor =
+              isSelected ? CupertinoColors.white : CupertinoColors.systemGrey;
+        });
+      },
+      child: ConstrainedBox(
+        constraints: BoxConstraints(
+          minWidth: 85,
+          maxWidth: isSelected ? double.infinity : 120,
         ),
-        child: Row(
-          mainAxisSize:
-              MainAxisSize.min, // Ensure the row takes only necessary width
-          children: [
-            Container(
-              margin: const EdgeInsets.only(right: 6),
-              child: const Icon(
-                CupertinoIcons.tag,
-                size: 16,
-                color: CupertinoColors.systemGrey,
-              ),
-            ),
-            Flexible(
-              // Use Flexible to adapt to the available space
-              child: Text(
-                widget.name,
-                style: const TextStyle(
-                  color: CupertinoColors.systemGrey,
-                  fontSize: 16,
+        child: AnimatedContainer(
+          duration: Duration(milliseconds: transitionDuration),
+          padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+          margin: const EdgeInsets.only(bottom: 12, left: 4, right: 4),
+          decoration: BoxDecoration(
+            color: backgroundColor,
+            borderRadius: BorderRadius.circular(20),
+          ),
+          child: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              AnimatedSwitcher(
+                duration: Duration(
+                    milliseconds:
+                        transitionDuration), // Duration of the transition
+                child: Icon(
+                  isSelected
+                      ? CupertinoIcons.clear_circled
+                      : CupertinoIcons.tag,
+                  key: ValueKey<bool>(isSelected), // Unique key for transition
+                  size: 16,
+                  color: textColor,
                 ),
-                overflow: TextOverflow.ellipsis,
-                maxLines: 1,
               ),
-            ),
-          ],
+              const SizedBox(width: 6),
+              Flexible(
+                child: AnimatedSwitcher(
+                    duration: Duration(milliseconds: transitionDuration),
+                    child: Text(
+                      widget.name,
+                      style: TextStyle(
+                        color: isSelected
+                            ? CupertinoColors.white
+                            : CupertinoColors.systemGrey,
+                        fontSize: 16,
+                      ),
+                      key: ValueKey<bool>(isSelected),
+                      overflow: TextOverflow.ellipsis,
+                      maxLines: 1,
+                    )),
+              ),
+            ],
+          ),
         ),
       ),
     );
