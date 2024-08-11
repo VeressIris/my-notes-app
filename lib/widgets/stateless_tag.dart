decbae0b2cb51003de61d2d51235148d8d8908dc
Iris Veress <irisveress@gmail.com>
2024-08-11 17:15:39 +0300

    refactored stless tag widget

--- lib/widgets/stateless_tag.dart
+++ lib/widgets/stateless_tag.dart
@@ -7,43 +7,61 @@ class StatelessTag extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final bool showTag = name != "No tags";
+
     return ConstrainedBox(
-        constraints: const BoxConstraints(
-          maxWidth: 120,
+      constraints: const BoxConstraints(maxWidth: 120),
+      child: Container(
+        padding: _getPadding(showTag),
+        decoration: _getDecoration(showTag),
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            if (showTag) _buildTagIcon(),
+            if (showTag) const SizedBox(width: 6),
+            Flexible(child: _buildTagName()),
+          ],
         ),
-        child: Container(
-          padding: name != "No tags" ? const EdgeInsets.symmetric(horizontal: 8, vertical: 2) : const EdgeInsets.all(0),
-          decoration: name != "No tags" ? BoxDecoration(
+      ),
+    );
+  }
+
+  EdgeInsets _getPadding(bool showTag) {
+    return showTag
+        ? const EdgeInsets.symmetric(horizontal: 8, vertical: 2)
+        : const EdgeInsets.all(0);
+  }
+
+  BoxDecoration _getDecoration(bool showTag) {
+    return showTag
+        ? BoxDecoration(
             color: CupertinoColors.darkBackgroundGray,
             borderRadius: BorderRadius.circular(20),
             border: Border.all(
               color: CupertinoColors.systemGrey,
               width: 1,
             ),
-          ) : const BoxDecoration(),
-          child: Row(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              name != "No tags"
-                  ? const Icon(
-                      CupertinoIcons.tag,
-                      size: 16,
-                      color: CupertinoColors.systemGrey,
-                    )
-                  : const SizedBox(),
-              name != "No tags" ? const SizedBox(width: 6) : const SizedBox(),
-              Flexible(
-                  child: Text(
-                name,
-                style: const TextStyle(
-                  color: CupertinoColors.systemGrey,
-                  fontSize: 16,
-                ),
-                overflow: TextOverflow.ellipsis,
-                maxLines: 1,
-              )),
-            ],
-          ),
-        ));
+          )
+        : const BoxDecoration();
+  }
+
+  Widget _buildTagIcon() {
+    return const Icon(
+      CupertinoIcons.tag,
+      size: 16,
+      color: CupertinoColors.systemGrey,
+    );
+  }
+
+  Widget _buildTagName() {
+    return Text(
+      name,
+      style: const TextStyle(
+        color: CupertinoColors.systemGrey,
+        fontSize: 16,
+      ),
+      overflow: TextOverflow.ellipsis,
+      maxLines: 1,
+    );
   }
 }
